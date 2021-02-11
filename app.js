2186af157d5599a9f5659bc3ba8404df97a91ab7
fthompson24 <fraserjthompson@gmail.com>
2021-02-10 18:37:04 -0600

    code snapshot

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArgs => {
-    for (let i=0; i < profileDataArgs.length; i+=1) {
-        console.log(profileDataArgs[i]);
-    }
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
 
-    console.log.apply("===========");
+  console.log('================');
 
-    profileDataArgs.forEach((profileItem) => 
-        console.log(profileItem));
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
+
 printProfileData(profileDataArgs);
