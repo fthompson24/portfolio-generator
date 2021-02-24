adda91dc7ce97759cee49263d48aad530a64f354
fthompson24 <fraserjthompson@gmail.com>
2021-02-23 22:00:28 -0600

    finished portfolio HTML output

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
+//const pageHTML = generatePage(name, github);
 
 
 // fs.writeFile('index.html', pageHTML, err => {
@@ -144,9 +144,17 @@ const promptProject = portfolioData => {
 };
 
 
+
+
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
 
